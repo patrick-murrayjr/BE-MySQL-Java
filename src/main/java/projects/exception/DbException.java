ac1371ce3eff24c21b9ee9cf6d6209f7251cdf4e
Patrick Murray <patrick.murrayjr@gmail.com>
2024-01-22 13:54:10 -0500

    Updated Repo for BE Week 7 MySQL-JDBC

---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package projects.exception;
+
+/**
+ * 
+ */
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+	/**
+	 * @param message
+	 */
+	public DbException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public DbException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
